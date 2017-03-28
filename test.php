c37df392b993f99b54f0b932a8e876f7765ac9f0
azics68 <etienne_christian@hotmail.com>
2017-03-28 18:09:19 +0200

    Add files via upload

--- test.php
+++ test.php
@@ -1,3 +1,13 @@
-<?php
-
-echo '<h1>Bonjour tout le monde</h1>';
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Ma page web</title>
+    </head>
+    <body>
+        <h1>Ma page web</h1>
+        <p>Aujourd'hui nous sommes le 
+        <?php echo date('d/m/Y h:i:s');  ?>.</p>
+        <p> <?php echo "Cette ligne a été écrite \"uniquement\" en PHP."; ?> </p>
+    </body>
+</html>
